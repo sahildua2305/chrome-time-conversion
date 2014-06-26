c07b9de519d5969334a21c5d7531e67de523ccf9
Sahil <sahildua2305@gmail.com>
2014-06-27 02:36:11 +0530

    internal CSS and working js finally

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 
 function toDate(){
 	var timestamp = document.getElementById('timetamp').value;
-	date = new Date(+timestamp);
+	date = new Date(+timestamp*1000);
 	
 	document.getElementById('date-format').value = date;
 }
 
-document.getElementById("convert1").addEventListener("click",toDate);
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('convert1').addEventListener('click', toDate);
+});
