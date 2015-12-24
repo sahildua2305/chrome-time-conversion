036bffb6607b91bf25a8b924d06030dea375924f
sahildua2305 <sahildua2305@gmail.com>
2015-12-25 01:30:09 +0530

    Added color select function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,39 @@
 
+$(document).ready(function(){
+
+	$("input").keypress(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+			toDate();
+		}
+	});
+
+	$(document).on('change','#colorselect',function(){
+		var color = $("#colorselect").val();
+		console.log(color);
+		if(color == "white"){
+			$("body").css("background-color", "#eee");
+			$("body").css("color", "#000");
+		}
+		else if(color == "grey"){
+			$("body").css("background-color", "grey");
+			$("body").css("color", "#000");
+		}
+		else if(color == "black"){
+			$("body").css("background-color", "#000");
+			$("body").css("color", "#eee");
+		}
+		else if(color == "orange"){
+			$("body").css("background-color", "orange");
+			$("body").css("color", "#000");
+		}
+		else if(color == "cyan"){
+			$("body").css("background-color", "cyan");
+			$("body").css("color", "#000");
+		}
+	});
+});
+
 function toDate(){
 	var timestamp = document.getElementById('timetamp').value;
 	date = new Date(+timestamp*1000);
