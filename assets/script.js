c0c6d8b4381b8c90873cf54237dbdf14ac80931f
Prabhakar Gupta <prabhakargupta267@gmail.com>
2015-12-25 16:55:19 +0530

    change color added

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,31 @@ $(document).ready(function(){
 			changeModeValue(1);
 		}
 	});
+
+	$(document).on('change','#colorselect',function(){
+		var color = $("#colorselect").val();
+		console.log(color);
+		if(color == "white"){
+			$("body").css("background-color", "#eee");
+			$("body").css("color", "#000");
+		}
+		else if(color == "grey"){
+			$("body").css("background-color", "grey");
+			$("body").css("color", "#000");
+		}
+		else if(color == "black"){
+			$("body").css("background-color", "#000");
+			$("body").css("color", "#eee");
+		}
+		else if(color == "orange"){
+			$("body").css("background-color", "orange");
+			$("body").css("color", "#000");
+		}
+		else if(color == "cyan"){
+			$("body").css("background-color", "cyan");
+			$("body").css("color", "#000");
+		}
+	});
 });
 
 function mainFunction(){
